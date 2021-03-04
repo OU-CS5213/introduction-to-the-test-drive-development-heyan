823d1a19812d206c2e96143691ea8f6bbef2cb32
heyan <Yan.He-2@ou.edu>
2021-03-04 00:53:07 -0600

    Add maven

---
+++ src/test/java/com/heyan/testdd/AWSTest.java
@@ -0,0 +1,137 @@
+package com.heyan.testdd;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class AWSTest {
+
+    private static final int FILLER_VALUE = Integer.MIN_VALUE;
+    private int[] original={1, 2, 3};
+    AWS originalAWS;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        originalAWS = new AWS(this.original);
+    }
+
+    @Test
+    void testGetValues() {
+        int[] value_expected = {1, 2, 3};
+        AWS aws = new AWS(value_expected);
+        int[] actual = aws.getValues();
+        assertEquals(actual[1], value_expected[1]);
+
+    }
+
+    @Test
+    void testSetValues() {
+        int[] value_expected = {1, 2, 3};
+        int[] readytoset = {1, 2, 4};
+        AWS aws = new AWS(value_expected);
+        aws.setValues(readytoset);
+        int[] actual = aws.getValues();
+        assertEquals(actual[2], 4);
+    }
+
+    @Test
+    void testToString() {
+        int[] value_expected = {1, 2, 3};
+        AWS aws = new AWS(value_expected);
+        String test_output = aws.toString();
+        assertEquals(test_output, "AWS [values=[1, 2, 3]]");
+    }
+
+    @Test
+    void testAWS() {
+        int[] expected = {1, 2, 3};
+        int[] x = {1, 2, 3};
+        AWS aws = new AWS(x);
+        x[1] = 100;
+
+        int[] actual = aws.getValues();
+        assertEquals(actual[0], expected[0]);
+        assertEquals(actual[1], expected[1]);
+    }
+
+    @Test
+    void testRemove() {
+        int[] x = {1, 2, 3};
+        AWS aws = new AWS(x);
+
+        int value = aws.remove(-1);
+        int expected = FILLER_VALUE;
+        assertEquals(expected, value);
+
+        value = aws.remove(x.length + 10);
+        expected = FILLER_VALUE;
+        assertEquals(expected, value);
+
+        value = aws.remove(0);
+        assertEquals(x[0], value);
+
+        int[] r = aws.getValues();
+        value = r[r.length - 1];
+        assertEquals(expected, value);
+
+        value = aws.remove(2);
+        assertEquals(r[2], value);
+
+        r = aws.getValues();
+        value = r[r.length - 1];
+        assertEquals(expected, value);
+
+    }
+
+    @Test
+    void testFillAndExpand() {
+        int position = 1;
+        int numberOfTimes = 2;
+        int[] org = originalAWS.getValues();
+        int expectedValue = org[position];
+        int first = org[0];
+
+        int expected = originalAWS.getValues().length + numberOfTimes;
+        originalAWS.fillAndExpand(position, numberOfTimes);
+        int[] result = originalAWS.getValues();
+        assertEquals(expected, result.length);
+
+        int a = result[1];
+        int b = result[2];
+        int c = result[3];
+        assertEquals(expectedValue, a);
+        assertEquals(expectedValue, b);
+        assertEquals(expectedValue, c);
+        assertEquals(first, result[0]);
+
+
+
+    }
+    @Test
+    void testFillAndExpandWithNegative() {
+        int position = 1;
+        int numberOfTimes = -2;
+
+        int[] org = originalAWS.getValues();
+        int expectedValue = org[position];
+        int first = org[0];
+        int expected = originalAWS.getValues().length - numberOfTimes;
+        originalAWS.fillAndExpand(position, numberOfTimes);
+        int[] result = originalAWS.getValues();
+        assertEquals(expected, result.length);
+
+        int a = result[1];
+        int b = result[2];
+        int c = result[3];
+        assertEquals(expectedValue, a);
+        assertEquals(expectedValue, b);
+        assertEquals(expectedValue, c);
+
+        assertEquals(first, result[0]);
+
+
+    }
+
+
+}
